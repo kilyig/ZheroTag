9a6e34c723a5161443fb16f3a2be8fe989e5390f
kilyig <yigit@kilicoglu.nom.tr>
2023-03-18 00:19:05 -0400

    DTao's random BigInts

--- test/utils/math-utils.ts
+++ test/utils/math-utils.ts
@@ -1,3 +1,13 @@
+import { BigNumber, utils } from "ethers";
+
+// from https://github.com/sigmachirality/empty-house/blob/main/frontend/src/utils/sampler.ts
+// Generate a random number from 2 to R in bigint
+export const sampleFieldElement = () => {
+    const randomHex = utils.randomBytes(8);
+    const randomNum = BigNumber.from(randomHex).toBigInt();
+    return randomNum;
+}
+
 export function randomInt() {
     return Math.floor(Math.random() * 100000);
 }
